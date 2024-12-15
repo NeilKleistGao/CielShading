a6b97cedf0496cd601ff5a00a9e193d380956a81
NeilKleistGao <neilkleist@gmail.com>
2024-12-15 19:41:55 +0800

    Render

--- ciel/CelShading.py
+++ ciel/CelShading.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import bpy
+import os
 
 class RenderType(Enum):
   COLOR = 0
@@ -12,6 +13,35 @@ class CelShadingOperator(bpy.types.Operator):
   bl_label = "Cel Shading Rendering"
   bl_options = { "REGISTER", "UNDO" }
 
-  def execute(self, context):
-    print("TODO")
+  base_output_dir = ""
+
+  def render_once(self, path: str, render_type: RenderType, context: bpy.types.Context | None):
+    path = os.path.join(self.base_output_dir, path)
+    print("output: " + path)
+    context.scene.render.filepath = path
+
+    origin_engine = context.scene.render.engine
+    origin_lighting = context.scene.display.shading.light
+    origin_studio_light = context.scene.display.shading.studio_light
+    if render_type == RenderType.COLOR:
+      context.scene.render.engine = "CYCLES" # TODO: config
+      context.scene.display.shading.light = "STUDIO"
+    else:
+      context.scene.render.engine = "BLENDER_WORKBENCH"
+      context.scene.display.shading.light = "MATCAP"
+      context.scene.display.shading.studio_light = "check_normal+y.exr"
+
+    context.scene.render.film_transparent = True
+    context.scene.render.image_settings.file_format = "PNG"
+    bpy.ops.render.render(animation=True)
+
+    context.scene.render.filepath = self.base_output_dir
+    context.scene.render.engine = origin_engine
+    context.scene.display.shading.light = origin_lighting
+    context.scene.display.shading.studio_light = origin_studio_light
+
+  def execute(self, context: bpy.types.Context | None):
+    self.base_output_dir = context.scene.render.filepath
+    self.render_once(context.scene.color_texture_output, RenderType.COLOR, context)
+    self.render_once(context.scene.normal_map_output, RenderType.NORMAL_MAP, context)
     return { "FINISHED" }
